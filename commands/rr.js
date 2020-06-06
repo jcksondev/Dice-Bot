c115aa8d769c128628684a2fa08473f623b764d9
JcksonDev <65576130+JcksonDev@users.noreply.github.com>
2020-06-06 15:02:14 -0700

    Added Advantage/Disadvantage

--- commands/rr.js
+++ commands/rr.js
@@ -7,28 +7,56 @@ module.exports = {
         const arg = args.toString().toLowerCase().replace(/\s/g, '');
         const command = arg.replace(/[,]/g, '');
 
-        // Create a random number between 1 and 20, or a d20 roll
-        const roll = _.random(1, 20);
+        // Checks input for 'a', 'adv', 'd', 'dis', creates a boolean value for adv/dis, and instantiates the rolls and total
+        const advantage = command.includes('adv') || command.includes('a');
+        const disadvantage = command.includes('dis') || command.includes('d');
+        let roll = 0;
+        let rolls = null;
+        let total = 0;
+
+        // Creates an array with two potential sizes for adv/dis
+        if(advantage || disadvantage) {
+            rolls = new Array(2).fill(0);
+        }else {
+            rolls = new Array(1).fill(0);
+        }
+
+        // Rolls a d20 or two dependant on adv/dis
+        rolls.forEach((val, index) => {
+            rolls[index] = _.random(1, 20);
+        });
 
         // Creates two fields for potential arguments, scans for a - or + and creates a value dependant on what is found. Defaults to 0
         const penalty = parseInt(command.split(/[-]/g)[1], 10) || 0;
         const bonus = parseInt(command.split(/[+]/g)[1], 10) || 0;
 
-        const total = roll - penalty + bonus;
+        // Calculate the advantage/disadvantage number and output, else output the normal number with modifier
+        const max = rolls.reduce((a, b) => {
+            return Math.max(a, b);
+        });
+
+        const min = rolls.reduce((a, b) => {
+            return Math.min(a, b);
+        });
+
+        if(advantage) {total = max - penalty + bonus; roll = max;}
+        else if(disadvantage) {total = min - penalty + bonus; roll = min;}
+        else {total = rolls.reduce((r, v) => r += v) - penalty + bonus; roll = rolls.reduce((r, v) => r += v);}
 
         // Custom messages if user gets a Natural 1/20 and the default message output
         const nat1 = '**Crtitical Fail!**';
         const nat20 = '**Natural 20!**';
         const returnMessages = [
-            `**Rolls**: [ ${roll} ] ${penalty ? `- ${penalty}` : ''} ${bonus ? `+ ${bonus}` : ''}\n**Total**: ${total}`,
+            `**Rolls**: [ ${rolls.join(', ')} ] ${penalty ? `- ${penalty}` : ''} ${bonus ? `+ ${bonus}` : ''}\n**Total**: ${total}`,
         ];
 
-        if(roll === 20) {
-            message.channel.send(`${returnMessages} \n${nat20}`);
-        }else if(roll === 1) {
-            message.channel.send(`${returnMessages} \n${nat1}`);
+        // Final output
+        if(roll === 1) {
+            message.channel.send(`${returnMessages}\n${nat1}`);
+        }else if(roll === 20) {
+            message.channel.send(`${returnMessages}\n${nat20}`);
         }else {
             message.channel.send(`${returnMessages}`);
         }
-    }
+    },
 };
